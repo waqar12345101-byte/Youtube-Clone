09a6a7d27ae134fffae3b23a2c357e635b83f6b8
Waqar <waqar12345101@gmail.com>
2026-09-23 16:47:23 +0500

    Working

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express"
+ const app = expresss()
+
+
+
+
+ export {app}
--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);       
+    })
+})
+.catch((err)=>{
+    console.log("Error",err);
+    
+})
 
 
 /*import express from "express"
