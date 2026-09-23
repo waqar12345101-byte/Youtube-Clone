02ea53f417717a0769f3b1257aa46923018d25b0
Waqar <waqar12345101@gmail.com>
2026-09-23 23:45:49 +0500

    Working

--- src/utils/asynchandling.js
+++ src/utils/asynchandling.js
@@ -1,8 +1,6 @@
 const asynchandler = (requestHandle) => {
-    (req,res,next) => {
+   return (req,res,next) => {
         Promise.resolve(requestHandle(req,res,next)).catch((err)=>next(err))
     }
 }
-
-
 export {asynchandler}
